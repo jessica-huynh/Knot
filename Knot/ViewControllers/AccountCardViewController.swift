08b27847f8deca7df2207a83584fdb45411d02dc
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-03-11 15:41:42 -0400

    Show bank account details in account card view

---
+++ Knot/ViewControllers/AccountCardViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AccountCardViewController.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-11.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class AccountCardViewController: UIViewController {
+
+    var account: Account?
+    
+    // MARK: - Outlets
+    @IBOutlet weak var accountCard: AccountCard!
+    @IBOutlet weak var dateAddedLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dateAddedLabel.text = "Added on Jan 11, 2020"
+        updateCard()
+    }
+    
+    // MARK: - Actions
+    @IBAction func deleteAccount(_ sender: UIButton) {
+    }
+    
+    func updateCard() {
+        accountCard.institutionLabel.text = account?.institution
+        accountCard.accountTypeLabel.text = "Chequing"
+        accountCard.accountNumberLabel.text = account?.accountNumber
+        accountCard.nameLabel.text = "Jane Doe"
+        //accountCard.logoImage.image = UIImage(named: "Logo")
+    }
+
+}
---
+++ Knot/Views/AccountCard.swift
@@ -0,0 +1,51 @@
+//
+//  AccountCard.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-11.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class AccountCard: UIView {
+
+    // MARK: - Outlets
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var institutionLabel: UILabel!
+    @IBOutlet weak var accountTypeLabel: UILabel!
+    @IBOutlet weak var accountNumberLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        Bundle.main.loadNibNamed("AccountCard", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleRightMargin]
+        
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let boxWidth: CGFloat = bounds.size.width - 1
+        let boxHeight: CGFloat = bounds.size.height - 1
+      
+        let boxRect = CGRect(x: 0, y: 0, width: boxWidth, height: boxHeight)
+        let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
+        UIColor.systemRed.setStroke()
+        roundedRect.stroke()
+    }
+
+
+}
