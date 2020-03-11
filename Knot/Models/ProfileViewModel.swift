d697c6a0c9b6001745646503812c177d36884b64
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-03-10 21:48:52 -0400

    Add view model for table view in profile scene

---
+++ Knot/Models/ProfileViewModel.swift
@@ -0,0 +1,114 @@
+//
+//  ProfileViewModel.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-10.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Temp account data model
+struct Account {
+    let institution: String
+    let accountNumber: String
+}
+
+
+protocol ProfileViewModelSection {
+    var type: ProfileViewModel.SectionType { get }
+    var title: String { get }
+    var rowCount: Int { get }
+}
+
+class ProfileViewModel: NSObject {
+    enum SectionType {
+        case accounts
+        case miscButtons
+    }
+    
+    var sections = [ProfileViewModelSection]()
+    
+    override init() {
+        super.init()
+
+        // Fake account data
+        let account1 = Account(institution: "Scotiabank", accountNumber: "**** **** 4316")
+        let account2 = Account(institution: "Tangerine", accountNumber: "**** **** 8625")
+        let account3 = Account(institution: "Simplii", accountNumber: "**** **** 2906")
+        let accounts = [account1, account2, account3]
+        
+        sections.append(Accounts(accounts: accounts))
+        sections.append(MiscButtons())
+    }
+}
+
+// MARK: - Table View Data Source
+extension ProfileViewModel: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].rowCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = sections[indexPath.section]
+        
+        switch section.type {
+        case .accounts:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AccountNumberCell", for: indexPath)
+            let section = section as! Accounts
+            cell.textLabel?.text = section.accounts[indexPath.row].institution
+            cell.detailTextLabel?.text = section.accounts[indexPath.row].accountNumber
+            return cell
+        case .miscButtons:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BasicProfileCell", for: indexPath)
+            let section = section as! MiscButtons
+            cell.textLabel?.text = section.buttons[indexPath.row]
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
+}
+
+// MARK: - View Model Data
+class Accounts: ProfileViewModelSection {
+    var accounts: [Account]
+    
+    var type: ProfileViewModel.SectionType {
+        return .accounts
+    }
+    
+    var title: String {
+        return "Accounts"
+    }
+    
+    var rowCount: Int {
+        return accounts.count
+    }
+    
+    init(accounts: [Account]) {
+        self.accounts = accounts
+    }
+}
+
+class MiscButtons: ProfileViewModelSection {
+    let buttons = ["Logout", "Plaid API"]
+    var type: ProfileViewModel.SectionType {
+        return .miscButtons
+    }
+    
+    var title: String {
+        return " "
+    }
+    
+    var rowCount: Int {
+        return buttons.count
+    }
+}
