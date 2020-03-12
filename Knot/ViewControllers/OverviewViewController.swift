ad1b8a16299c32f97f46871f402bdf8faa9fc5f0
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-03-12 19:06:18 -0400

    Add collection view for viewing recent transactions

--- Knot/ViewControllers/OverviewViewController.swift
+++ Knot/ViewControllers/OverviewViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class OverviewViewController: UITableViewController {
-
+    
+    let maxTransactionsDisplayed = 6
+    let lastTransactionIndex = 6
+    var transactions: [Transaction]!
+    
     // MARK: - Outlets
     @IBOutlet weak var netBalanceLabel: UILabel!
     @IBOutlet weak var cashBalanceLabel: UILabel!
     @IBOutlet weak var creditCardsBalanceLabel: UILabel!
     @IBOutlet weak var investmentsBalanceLabel: UILabel!
+    @IBOutlet weak var transactionCollectionView: UICollectionView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        transactionCollectionView.register(UINib(nibName: "TransactionCollectionCell", bundle: nil), forCellWithReuseIdentifier: "TransactionCollectionCell")
+        transactionCollectionView.dataSource = self
+        transactionCollectionView.delegate = self
+        transactionCollectionView.showsHorizontalScrollIndicator = false
+        
         updateLabels()
+        
+        let transaction1 = Transaction(description: "Uber", date: Date(), amount: 12.45)
+        let transaction2 = Transaction(description: "Walmart", date: Date(), amount: 38.12)
+        let transaction3 = Transaction(description: "Transfer", date: Date(), amount: -50.00)
+        let transaction4 = Transaction(description: "Transfer", date: Date(), amount: -50.00)
+        let transaction5 = Transaction(description: "Transfer", date: Date(), amount: -50.00)
+        let transaction6 = Transaction(description: "Transfer", date: Date(), amount: -50.00)
+        transactions = [transaction1, transaction2, transaction3, transaction4, transaction5, transaction6, transaction1]
     }
     
     // MARK: - Actions
@@ -52,5 +71,25 @@ class OverviewViewController: UITableViewController {
             }
         }
     }
+}
+
+// MARK: - Collection View Extension
+extension OverviewViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (transactions.count > maxTransactionsDisplayed ?
+                maxTransactionsDisplayed + 1 : maxTransactionsDisplayed)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == lastTransactionIndex {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ViewMoreTransactionsCell", for: indexPath)
+            return cell
+        }
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TransactionCollectionCell", for: indexPath) as! TransactionCollectionCell
+        cell.configure(for: transactions[indexPath.item])
+        return cell
+    }
+    
 }
 
---
+++ Knot/Views/TransactionCollectionCell.swift
@@ -0,0 +1,27 @@
+//
+//  TransactionCollectionCell.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-12.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class TransactionCollectionCell: UICollectionViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(for transaction: Transaction) {
+        amountLabel.text = "$\(transaction.amount)"
+        descriptionLabel.text = transaction.description
+        
+        self.contentView.layer.cornerRadius = 10
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+    }
+}
