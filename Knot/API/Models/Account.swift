769549e1a20ff385a2a8d1eeb5a4968ee3f137f6
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-03-21 21:03:26 -0400

    Add account and institution models

---
+++ Knot/API/Models/Account.swift
@@ -0,0 +1,35 @@
+//
+//  Account.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-21.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import Foundation
+
+struct Account: Codable, CustomStringConvertible {
+    let id, name, subtype: String
+    let type: AccountType
+    let mask, officialName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name, mask, type, subtype
+        case id = "account_id"
+        case officialName = "official_name"
+    }
+    
+    enum AccountType: String, Codable, Equatable {
+        case investment, credit, depository, loan, other
+    }
+    
+    var description: String {
+        return "Account ID: \(id), name: \(name), type: \(type)"
+    }
+}
+
+extension Account {
+    init(data: Data) throws {
+        self = try JSONDecoder().decode(Account.self, from: data)
+    }
+}
---
+++ Knot/API/Models/Institution.swift
@@ -0,0 +1,24 @@
+//
+//  Institution.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-21.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import Foundation
+
+struct Institution: Codable {
+    let name, id: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case id = "institution_id"
+    }
+}
+
+extension Institution {
+    init(data: Data) throws {
+        self = try JSONDecoder().decode(Institution.self, from: data)
+    }
+}
