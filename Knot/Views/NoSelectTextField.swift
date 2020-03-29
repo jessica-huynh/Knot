ab543640970b0f524f4d0c000eb950c164edb4c3
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-03-29 16:47:29 -0400

    Add custom UITextField that is not selectable

---
+++ Knot/Views/NoSelectTextField.swift
@@ -0,0 +1,23 @@
+//
+//  NoSelectTextField.swift
+//  Knot
+//
+//  Created by Jessica Huynh on 2020-03-29.
+//  Copyright © 2020 Jessica Huynh. All rights reserved.
+//
+
+import UIKit
+
+class NoSelectTextField: UITextField {
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return false
+    }
+    
+    override func caretRect(for position: UITextPosition) -> CGRect {
+        return CGRect.zero
+    }
+    
+    override func selectionRects(for range: UITextRange) -> [UITextSelectionRect] {
+        return []
+    }
+}
