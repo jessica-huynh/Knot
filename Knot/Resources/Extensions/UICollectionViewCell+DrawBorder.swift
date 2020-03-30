29ff5afe476340fefd8814a891c3754cbad79b81
Jessica Huynh <jessica.tr.huynh@gmail.com>
2020-03-30 13:27:17 -0400

    Add line dash pattern option when drawing border

--- Knot/Resources/Extensions/UICollectionViewCell+DrawBorder.swift
+++ Knot/Resources/Extensions/UICollectionViewCell+DrawBorder.swift
@@ -10,10 +10,17 @@ import Foundation
 import UIKit
 
 extension UICollectionViewCell {
-    func drawBorder(with cornerRadius: CGFloat = 10.0, width: CGFloat = 1.0, colour: CGColor = UIColor.lightGray.cgColor) {
+    func drawBorder(with cornerRadius: CGFloat = 10.0, width: CGFloat = 1.0, colour: CGColor = UIColor.lightGray.cgColor, lineDashPattern: [NSNumber]? = nil) {
         self.contentView.layer.cornerRadius = cornerRadius
-        self.contentView.layer.borderWidth = width
-        self.contentView.layer.borderColor = colour
         self.contentView.layer.masksToBounds = true
+        
+        let border = CAShapeLayer()
+        border.frame = self.contentView.bounds
+        border.fillColor = nil
+        border.strokeColor = colour
+        border.lineWidth = width
+        border.lineDashPattern = lineDashPattern
+        border.path = UIBezierPath(roundedRect: self.contentView.bounds, cornerRadius: cornerRadius).cgPath
+        self.contentView.layer.addSublayer(border)
     }
 }
